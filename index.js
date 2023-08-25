b4a66509f8333ea21641a3370602b29346fb58f1
HarshPreet <harshpreet3006@gmail.com>
2023-08-25 15:59:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+let express = require("express");
+let path = require("path") ;
+let mongoose = require("mongoose")
+let app = express() ;
+const ejs = require("ejs") ;
+let methodoverride = require("method-override")
+const bcrypt = require("bcrypt")
+require("./db/conn.js")
+app.use(express.json());
+const static_path = path.join(__dirname,"/public") 
+app.use(express.urlencoded({extended:false}))
+app.use(express.static(static_path))
+//app.set("view engine","ejs")
+app.use(methodoverride('_method'))
+const mongoUrl1 = 'mongodb://127.0.0.1:27017'; // Replace with your first MongoDB connection URL
+require("./db/conn.js")
+//const mongoUrl2 = 'mongodb://localhost:27017/db2'; // Replace with your second MongoDB connection URL
+const UsernameRegister = require("./models/username.js")
+
+app.use(express.static(__dirname));
+
+
+
+
+app.set('view engine','ejs') ;
+app.set(express.static('public')) ;
+
+app.get('/', function(req, res) {
+     UsernameRegister.find({})
+     .then((a)=>{
+        return res.render('index')
+        
+        
+     })
+     .catch((e)=>{
+        console.log(e) ;
+      })
+    
+  });
+  app.get('/', function(req, res) {
+    UsernameRegister.find({})
+    .then((a)=>{
+        res.json(a)
+        console.log(a) ;
+       
+    })
+    .catch((e)=>{
+       console.log(e) ;
+     })
+   
+ });
+ app.get('/register',(req,res)=>{
+    res.render('res.ejs')
+ })
+ app.post("/login",async(req,res)=>{
+    try{
+        const name = req.body.name;
+        const password = req.body.password ;
+        const username = await UsernameRegister.findOne({name:name})
+        const isMatch = bcrypt.compare(password,username.password)
+        if(isMatch){
+            res.json({ mesaage: 'LOgin Successfull' });
+        }
+    }catch(e){
+    console.log(e)
+    }
+ })
+
+ app.post('/register',async(req,res)=>{
+    let data= new UsernameRegister({ 
+        name : req.body.username,
+        password : req.body.password
+    
+    })
+    console.log(data) ;
+   const register = data.save()
+    .then((x)=>{
+        res.status(200).json(register);
+    })
+    .catch((e)=>{
+        console.log(e);
+    })
+})
+app.listen(400,()=>{
+    console.log("Port Working") ;
+})
---
+++ models/username.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+bcrypt = require("bcrypt") ;
+const jwt = require("jsonwebtoken")
+ const usernnameSchema = new mongoose.Schema({
+    name:{
+        type:String
+    },
+    password:{
+        type:String
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
+    
+ })
+  usernnameSchema.methods.generateAuthToken = async function(){
+    try{
+        const token = jwt.sign({id:this._id},"mynameisHarshpeet")
+        this.tokens  = this.tokens.concat({token:thapa})
+        console.log(token)
+        return token ;
+    }
+    catch{
+         res.send("error  "+error);
+         console.log("error "+error)
+    }
+  }
+ 
+ usernnameSchema.pre("save",async function(next){
+   // const passwordHash = await bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
+    console.log(this.password) ;
+    next();
+ })
+ const UsernameRegister = new mongoose.model("UsernameRegister",usernnameSchema);
+ module.exports = UsernameRegister ;
